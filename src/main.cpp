b4268ff309499e7274c0f85e2f108b8dee767231
Ananth <ananth@wix.com>
2020-10-31 15:58:50 -0500

    error static assertion failed std::thread arguments must be invocable after conversion to rvalues

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include "ncurses_display.h"
 #include "system.h"
+#include <thread>
+#include <iostream>
+
+auto f_dummy  = [](){ std::cout << "Test";};
 
 int main() {
   System system;
   NCursesDisplay::Display(system);
 
-  // debug
-  // system.Cpu().Utilization();
-  // system.Processes();
+  std::thread t_pinfo( &NCursesDisplay::Display, system );
+  t_pinfo.join();
+
+  // std::thread t1( [](){ std::cout << "Test";} ); // works fine
+  // t1.join();
+
 }
+
